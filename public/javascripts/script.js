db0c00b51b7b555ba65801aabad3b61879d90a0a
Mariah Dunn <mariahdunn@Mariahs-MacBook-Pro.local>
2018-07-14 16:40:10 -0400

    Updated nav bar.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+
+
+
+
+
+//NEED TO UPDATE ACTIVE NAV TAB PER PAGE
+// $('.tabular.menu .item').tab();
+
+// $('.item')
+
+// //Update Nav Bar Active class
+// const link = $("a[href$='']")
+
+// (function(e) {
+//   // e.preventDefault();
+//   $('.item').removeClass('active');
+//   $(this).addClass('active');
+// });
+
