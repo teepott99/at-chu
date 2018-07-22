52404515adfa82974fdfbb2018f37b878463cbef
Mariah Dunn <mariahdunn@Mariahs-MacBook-Pro.local>
2018-07-22 16:05:51 -0400

    Some geocoder progress made.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,71 @@
-document.addEventListener('DOMContentLoaded', () => {
+// //leaflet geosearch dependencies
+// const debounce = require('lodash.debounce');
+// const nodent = require('nodent-runtime');
+// var leafletGeosearch = require("leaflet-geosearch")
 
-}, false);
+// document.addEventListener('DOMContentLoaded', () => {
 
-$('.menu-class.sidebar')
-  .sidebar('setting', 'transition', 'overlay')
-  .sidebar('toggle')
-;
+// }, false);
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
+//Leaflet
+
+//Map 
+var mymap = L.map('mapid').setView([25.7617, -80.1918], 5);
+
+//Map Styles
 L.tileLayer('https://api.mapbox.com/styles/v1/teepott/cjjvtbvb30qi82rlah9vvh16o/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGVlcG90dCIsImEiOiJjamp2dDZxYXI5dGV6M2twMTlnMnlkdW12In0.6mzsSubKcXrowebXFnNzgg', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
+
+//Leaflet Geosearch Setup
+
+const { 
+  GeoSearchControl, 
+  OpenStreetMapProvider,
+} = window.GeoSearch;
+
+// const provider = new OpenStreetMapProvider();
+
+// const form = document.querySelector('form');
+// const input = form.querySelector('input[type="text"]');
+// const results = document.getElementById('results');
+
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   provider.search({ query: input.value }).then((results) => {
+//     console.log(results);
+//   });
+// });
+
+
+
+
+
+
+// var GeoSearchControl = window.GeoSearch.GeoSearchControl;
+// var OpenStreetMapProvider = window.GeoSearch.OpenStreetMapProvider;
+
+
+const provider = new OpenStreetMapProvider();
+ 
+const form = document.getElementById('postForm');
+const userInput = form.getElementById('userLocation');
+const tagInput = form.getElementById('tagLocation');
+ 
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+ 
+  const userResults = await provider.search({ query: userInput.value });
+  const tagResults = await provider.search({ query: tagInput.value });
+  console.log(userResults, tagResults); // » [{}, {}, {}, ...]
+});
+
+ 
 
 
 //NEED TO UPDATE ACTIVE NAV TAB PER PAGE
